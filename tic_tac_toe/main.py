e941a843155c0f7292724e15a2d5f17bc37c90d5
kalimullin_workspace <noti1996@gmail.com>
2022-03-01 16:02:04 +0300

    Наброски функций для крестик-нолики.

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -1,28 +1,91 @@
 import random
 
-game = [[0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8]]
+board = [['_', '_', '_'],
+         ['_', '_', '_'],
+         ['_', '_', '_']]
 
-wins = [[0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8]]
-
-def minmax(game, index, player):
-    pass
-
-def step(player):
-    pass
-
-def main():
-    pass
+def moving(board):
+    for row in board:
+        if '_' in row:
+            return True
+    return False
 
 
-if __name__ == '__main__':
-    x = random.randint(0, 8)
-    main()
+def check_win(board, player1, player2):
+    for row in range(0, 3):
+        if board[row][0] == board[row][1] and board[row][1] == board[row][2]:
+            if board[row][0] == player1:
+                return 10
+            elif board[row][0] == player2:
+                return -10
+
+    for col in range(0, 3):
+        if board[0][col] == board[1][col] and board[1][col] == board[2][col]:
+            if board[0][col] == player1:
+                return 10
+            elif board[0][col] == player2:
+                return -10
+
+    if (board[0][0] == board[1][1] and board[1][1] == board[2][2]):
+        if board[0][0] == player1:
+            return 10
+        elif board[0][0] == player2:
+            return -10
+
+    if (board[0][2] == board[1][1] and board[1][1] == board[2][0]):
+        if board[0][2] == player1:
+            return 10
+        elif board[0][2] == player2:
+            return -10
+    return 0
+
+
+def minimax(board, ismax, player1, player2):
+    score = check_win(board, player1, player2)
+
+    if score == 10:
+        return score
+
+    if score == -10:
+        return score
+
+    if moving(board) is False:
+        return 0
+
+    if ismax:
+        best = - 1500
+        for row in board:
+            for col in row:
+                if board[row][col] == '_':
+                    board[row][col] = player1
+                    best = max(best, minimax(board, not ismax, player1, player2))
+                    board[row][col] = '_'
+        return best
+    else:
+        best = 1500
+        for row in board:
+            for col in row:
+                if board[row][col] == '_':
+                    board[row][col] = player2
+                    best = min(best, minimax(board, not ismax, player1, player2))
+                    board[row][col] = '_'
+        return best
+
+
+def best(board, player1, player2):
+    bestVal = -1500
+    bestMove = (-1, -1)
+    for row in board:
+        for col in row:
+            if board[row][col] == '_':
+                board[row][col] = player1
+                currentVal = minimax(board, True, player1, player2)
+                board[row][col] = '_'
+                if currentVal > bestVal:
+                    bestVal = currentVal
+                    bestMove = (row, col)
+    return bestMove
+
+
+
+
